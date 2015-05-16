39c9483f8acc710fe9ce79758b0db693b81d1744
James Gomez <jim.gomez.48@gmail.com>
2015-05-15 18:28:44 -0700

    Updated load.sql to use xml data for all bulk load operations.

--- MovieDB/sql/load.sql
+++ MovieDB/sql/load.sql
@@ -1,19 +1,19 @@
 SET GLOBAL local_infile=1;
 
-LOAD DATA LOCAL INFILE './seed_data/actor1.del' INTO TABLE MovieDB_actor FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
-LOAD DATA LOCAL INFILE './seed_data/actor2.del' INTO TABLE MovieDB_actor FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
-LOAD DATA LOCAL INFILE './seed_data/actor3.del' INTO TABLE MovieDB_actor FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+LOAD XML LOCAL INFILE './seed_data/xml/actor1.xml' INTO TABLE MovieDB_actor;
+LOAD XML LOCAL INFILE './seed_data/xml/actor2.xml' INTO TABLE MovieDB_actor;
+LOAD XML LOCAL INFILE './seed_data/xml/actor3.xml' INTO TABLE MovieDB_actor;
 
-LOAD DATA LOCAL INFILE './seed_data/director.del' INTO TABLE MovieDB_director FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+LOAD XML LOCAL INFILE './seed_data/xml/director.xml' INTO TABLE MovieDB_director;
 
-LOAD DATA LOCAL INFILE './seed_data/movie.del' INTO TABLE MovieDB_movie FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+LOAD XML LOCAL INFILE './seed_data/xml/movie.xml' INTO TABLE MovieDB_movie;
 
-LOAD XML LOCAL INFILE './seed_data/movieactor1.xml' INTO TABLE MovieDB_movieactor;
-LOAD XML LOCAL INFILE './seed_data/movieactor2.xml' INTO TABLE MovieDB_movieactor;
+LOAD XML LOCAL INFILE './seed_data/xml/movieactor1.xml' INTO TABLE MovieDB_movieactor;
+LOAD XML LOCAL INFILE './seed_data/xml/movieactor2.xml' INTO TABLE MovieDB_movieactor;
 
-LOAD XML LOCAL INFILE './seed_data/moviedirector.xml' INTO TABLE MovieDB_moviedirector;
+LOAD XML LOCAL INFILE './seed_data/xml/moviedirector.xml' INTO TABLE MovieDB_moviedirector;
 
-LOAD XML LOCAL INFILE './seed_data/moviegenre.xml' INTO TABLE MovieDB_moviegenre;
+LOAD XML LOCAL INFILE './seed_data/xml/moviegenre.xml' INTO TABLE MovieDB_moviegenre;
 
 INSERT INTO MovieDB_maxpersonid VALUES (69000);
 INSERT INTO MovieDB_maxmovieid VALUES (4750);

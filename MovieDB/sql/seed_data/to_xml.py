d5ccf2a3c8532c2a086495150a0b16edb8acaf45
James Gomez <jim.gomez.48@gmail.com>
2015-05-15 18:15:07 -0700

    Created corresponding xml seed data files for all models and moved seed data into csv and xml directories

--- MovieDB/sql/seed_data/to_xml.py
+++ MovieDB/sql/seed_data/to_xml.py
@@ -3,6 +3,69 @@ import csv
 import os
 
 def main():
+    orig = 'actor1.del'
+    with open(orig, 'r') as infile:
+        root = ET.Element('root')
+        for tokens in csv.reader(infile):
+            row = ET.SubElement(root, 'row')
+            ET.SubElement(row, 'field', name='id').text = str(tokens[0]).decode('utf8')
+            ET.SubElement(row, 'field', name='last').text = str(tokens[1]).decode('utf8')
+            ET.SubElement(row, 'field', name='first').text = str(tokens[2]).decode('utf8')
+            ET.SubElement(row, 'field', name='sex').text = str(tokens[3]).decode('utf8')
+            ET.SubElement(row, 'field', name='dob').text = str(tokens[4]).decode('utf8')
+            ET.SubElement(row, 'field', name='dod').text = str(tokens[5]).decode('utf8')
+        tree = ET.ElementTree(root)
+        tree.write(orig[:-3] + 'xml')
+    orig = 'actor2.del'
+    with open(orig, 'r') as infile:
+        root = ET.Element('root')
+        for tokens in csv.reader(infile):
+            row = ET.SubElement(root, 'row')
+            ET.SubElement(row, 'field', name='id').text = str(tokens[0]).decode('utf8')
+            ET.SubElement(row, 'field', name='last').text = str(tokens[1]).decode('utf8')
+            ET.SubElement(row, 'field', name='first').text = str(tokens[2]).decode('utf8')
+            ET.SubElement(row, 'field', name='sex').text = str(tokens[3]).decode('utf8')
+            ET.SubElement(row, 'field', name='dob').text = str(tokens[4]).decode('utf8')
+            ET.SubElement(row, 'field', name='dod').text = str(tokens[5]).decode('utf8')
+        tree = ET.ElementTree(root)
+        tree.write(orig[:-3] + 'xml')
+    orig = 'actor3.del'
+    with open(orig, 'r') as infile:
+        root = ET.Element('root')
+        for tokens in csv.reader(infile):
+            row = ET.SubElement(root, 'row')
+            ET.SubElement(row, 'field', name='id').text = str(tokens[0]).decode('utf8')
+            ET.SubElement(row, 'field', name='last').text = str(tokens[1]).decode('utf8')
+            ET.SubElement(row, 'field', name='first').text = str(tokens[2]).decode('utf8')
+            ET.SubElement(row, 'field', name='sex').text = str(tokens[3]).decode('utf8')
+            ET.SubElement(row, 'field', name='dob').text = str(tokens[4]).decode('utf8')
+            ET.SubElement(row, 'field', name='dod').text = str(tokens[5]).decode('utf8')
+        tree = ET.ElementTree(root)
+        tree.write(orig[:-3] + 'xml')
+    orig = 'director.del'
+    with open(orig, 'r') as infile:
+        root = ET.Element('root')
+        for tokens in csv.reader(infile):
+            row = ET.SubElement(root, 'row')
+            ET.SubElement(row, 'field', name='id').text = str(tokens[0]).decode('utf8')
+            ET.SubElement(row, 'field', name='last').text = str(tokens[1]).decode('utf8')
+            ET.SubElement(row, 'field', name='first').text = str(tokens[2]).decode('utf8')
+            ET.SubElement(row, 'field', name='dob').text = str(tokens[3]).decode('utf8')
+            ET.SubElement(row, 'field', name='dod').text = str(tokens[4]).decode('utf8')
+        tree = ET.ElementTree(root)
+        tree.write(orig[:-3] + 'xml')
+    orig = 'movie.del'
+    with open(orig, 'r') as infile:
+        root = ET.Element('root')
+        for tokens in csv.reader(infile):
+            row = ET.SubElement(root, 'row')
+            ET.SubElement(row, 'field', name='id').text = str(tokens[0]).decode('utf8')
+            ET.SubElement(row, 'field', name='title').text = str(tokens[1]).decode('utf8')
+            ET.SubElement(row, 'field', name='year').text = str(tokens[2]).decode('utf8')
+            ET.SubElement(row, 'field', name='rating').text = str(tokens[3]).decode('utf8')
+            ET.SubElement(row, 'field', name='company').text = str(tokens[4]).decode('utf8')
+        tree = ET.ElementTree(root)
+        tree.write(orig[:-3] + 'xml')
     orig = 'movieactor1.del'
     with open(orig, 'r') as infile:
         root = ET.Element('root')

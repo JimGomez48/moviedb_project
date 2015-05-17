8e5c763fb91cdbaef4741466e67163a0aa9132bd
James Gomez <jim.gomez.48@gmail.com>
2015-05-17 04:02:53 -0700

    Registering models in admin app.

--- MovieDB/admin.py
+++ MovieDB/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(Movie)
+admin.site.register(Review)
+admin.site.register(MovieActor)
+admin.site.register(MovieDirector)
+admin.site.register(MovieGenre)
+admin.site.register(MaxPersonID)
+admin.site.register(MaxMovieID)

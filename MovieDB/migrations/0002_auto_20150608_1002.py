a7351374efd9db71182ce1472f4a58a12a4ebd90
James Gomez <jim.gomez.48@gmail.com>
2015-06-08 04:01:56 -0700

    Forgot to add new migration again.

---
+++ MovieDB/migrations/0002_auto_20150608_1002.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+from django.db import migrations
+from moviedb_project.settings import BASE_DIR
+
+
+def load_seed_data_sql():
+    print 'Loading seed data...'
+    sql = open(os.path.join(BASE_DIR, os.path.normpath('MovieDB/sql/scripts/load.sql')), 'r').read()
+    return sql
+
+
+def create_stored_procedures():
+    print 'Creating stored procs...'
+    PATH = os.path.join(BASE_DIR, os.path.normpath('MovieDB/sql/sprocs/'))
+    sql = ''
+    for file in os.listdir(PATH):
+        with open(os.path.join(PATH, file), 'r') as sproc:
+            sql += sproc.read()
+    return sql
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('MovieDB', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL(load_seed_data_sql()),
+        migrations.RunSQL(create_stored_procedures()),
+    ]

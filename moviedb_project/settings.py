7ee2d39127f7a8e04df4fa20271c246c944d1200
James Gomez <jim.gomez.48@gmail.com>
2015-11-19 01:36:10 -0800

    updated settings

--- moviedb_project/settings.py
+++ moviedb_project/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'movie_db',
-        'USER': 'james',
+        'USER': 'root',
         'PASSWORD': 'test',
         'HOST': '127.0.0.1',
         'PORT': '3306',
@@ -95,7 +95,7 @@ DATABASES = {
     'moviedb': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'movie_db',
-        'USER': 'james',
+        'USER': 'root',
         'PASSWORD': 'test',
         'HOST': '127.0.0.1',
         'PORT': '3306',
